4a39d0dc815f7980bea66ea0ded998921a41eb97
Pete Fritchman <petef@databits.net>
2023-05-09 21:13:05 +0000

    refactor

--- tests/test_blackjack_8_hit.py
+++ tests/test_blackjack_8_hit.py
@@ -11,7 +11,7 @@ from blackjack import Actions, Hand, Strategy
 from cards import Card, Ranks
 
 
-class TestHardHand:
+class TestHand:
     dealer_hands = [
         Hand([Card(Ranks._2)]),
         Hand([Card(Ranks._3)]),
@@ -26,56 +26,83 @@ class TestHardHand:
     ]
     strategy = Strategy(8, Actions.HIT)
 
-    # 5: hit against every dealer hand
-    def test_player_5(self):
-        player = Hand([Card(Ranks._2), Card(Ranks._3)])
+    def compare_actions(self, player, actions):
+        if len(actions) != len(self.dealer_hands):
+            raise Exception('mismatch between actions and dealer_hands length')
 
-        for dealer in self.dealer_hands:
-            action = self.strategy.decide(dealer, player)
-            assert action == Actions.HIT, f'player hard 5, dealer {dealer.cards[0].rank}'
-
-    # 6: hit against every dealer hand
-    def test_player_6(self):
-        player = Hand([Card(Ranks._2), Card(Ranks._4)])
-
-        for dealer in self.dealer_hands:
-            action = self.strategy.decide(dealer, player)
-            assert action == Actions.HIT, f'player hard 6, dealer {dealer.cards[0].rank}'
-
-    # 7: hit against every dealer hand
-    def test_player_7(self):
-        player = Hand([Card(Ranks._2), Card(Ranks._5)])
-
-        for dealer in self.dealer_hands:
-            action = self.strategy.decide(dealer, player)
-            assert action == Actions.HIT, f'player hard 7, dealer {dealer.cards[0].rank}'
-
-    # 8: hit against every dealer hand
-    def test_player_8(self):
-        player = Hand([Card(Ranks._2), Card(Ranks._6)])
-
-        for dealer in self.dealer_hands:
-            action = self.strategy.decide(dealer, player)
-            assert action == Actions.HIT, f'player hard 8, dealer {dealer.cards[0].rank}'
-
-    # 9: double on dealer 3-6, hit the rest
-    def test_player_9(self):
-        player = Hand([Card(Ranks._2), Card(Ranks._7)])
-
-        actions = [
-            Actions.HIT,    # dealer 2
-            Actions.DOUBLE, # dealer 3
-            Actions.DOUBLE, # dealer 4
-            Actions.DOUBLE, # dealer 5
-            Actions.DOUBLE, # dealer 6
-            Actions.HIT,    # dealer 7
-            Actions.HIT,    # dealer 8
-            Actions.HIT,    # dealer 9
-            Actions.HIT,    # dealer T
-            Actions.HIT,    # dealer A
-        ]
+        player_type, player_value = player.value()
 
         for i, expected_action in enumerate(actions):
             dealer = self.dealer_hands[i]
             action = self.strategy.decide(dealer, player)
-            assert action == expected_action, f'player hard 9, dealer {dealer.cards[0].rank}'
+            assert action == expected_action, f'player {player_type} {player_value}, ' \
+                                              f'dealer {dealer.cards[0].rank}'
+
+    # hard 5: hit against every dealer hand
+    def test_player_hard_5(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._3)])
+        assert player.value() == ('hard', 5)
+
+        self.compare_actions(player, [Actions.HIT] * 10)
+
+    # hard 6: hit against every dealer hand
+    def test_player_hard_6(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._4)])
+        assert player.value() == ('hard', 6)
+
+        self.compare_actions(player, [Actions.HIT] * 10)
+
+    # hard 7: hit against every dealer hand
+    def test_player_hard_7(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._5)])
+        assert player.value() == ('hard', 7)
+
+        self.compare_actions(player, [Actions.HIT] * 10)
+
+    # hard 8: hit against every dealer hand
+    def test_player_hard_8(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._6)])
+        assert player.value() == ('hard', 8)
+
+        self.compare_actions(player, [Actions.HIT] * 10)
+
+    # hard 9: double on dealer 3-6, hit the rest
+    def test_player_hard_9(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._7)])
+        assert player.value() == ('hard', 9)
+
+        self.compare_actions(player, [
+            Actions.HIT,     # dealer 2
+            Actions.DOUBLE,  # dealer 3
+            Actions.DOUBLE,  # dealer 4
+            Actions.DOUBLE,  # dealer 5
+            Actions.DOUBLE,  # dealer 6
+            Actions.HIT,     # dealer 7
+            Actions.HIT,     # dealer 8
+            Actions.HIT,     # dealer 9
+            Actions.HIT,     # dealer T
+            Actions.HIT,     # dealer A
+        ])
+
+    # hard 10: hit on dealer T/A, double the rest
+    def test_player_hard_10(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._8)])
+        assert player.value() == ('hard', 10)
+
+        self.compare_actions(player, [
+            Actions.DOUBLE,  # dealer 2
+            Actions.DOUBLE,  # dealer 3
+            Actions.DOUBLE,  # dealer 4
+            Actions.DOUBLE,  # dealer 5
+            Actions.DOUBLE,  # dealer 6
+            Actions.DOUBLE,  # dealer 7
+            Actions.DOUBLE,  # dealer 8
+            Actions.DOUBLE,  # dealer 9
+            Actions.HIT,     # dealer T
+            Actions.HIT,     # dealer A
+        ])
+
+    # hard 11: double on everything
+    def test_player_hard_11(self):
+        player = Hand([Card(Ranks._2), Card(Ranks._9)])
+        self.compare_actions(player, [Actions.DOUBLE] * 10)
