3092f611c8c8397ad73a41574790410efd5dc00a
Pete Fritchman <petef@databits.net>
2023-05-09 21:55:59 +0000

    finish strategy for hard-value hands

--- blackjack/strategy.py
+++ blackjack/strategy.py
@@ -24,29 +24,58 @@ class Strategy:
         dealer_card = dealer.cards[0]
         player_type, player_value = player.value()
 
+        # by default, we stand (final return). the following logic is to decide if we should
+        # do something other than hit
+
+        # it's easiest to think about the dealer hand value by assuming any card we can't
+        # see is valued at 10 (T, J, Q, K).
+        if dealer_card.rank == Ranks.A:
+            dealer_value = 21
+        else:
+            dealer_value = 10 + dealer_card.value()
+
         # if we have 2 cards, check for split & double scenarios
         if len(player.cards) == 2:
-            # splits
+            # deal with pairs
             if player.cards[0].rank == player.cards[1].rank:
                 match player.cards[0].rank:
                     case Ranks._8:
                         # always split 8s
                         return Actions.SPLIT
 
-            # doubles on hard values
+            # double down when we have a hard value hand
             if player_type == 'hard':
-                # player hard 9, double on dealer 3/4/5/6
-                if player_value == 9:
-                    if dealer_card.value() in [3, 4, 5, 6]:
+                match player_value:
+                    case 9:
+                        # player hard 9, double on dealer 3/4/5/6
+                        if 13 <= dealer_value <= 16:
+                            return Actions.DOUBLE
+                    case 10:
+                        # double 10 except against T/A
+                        if dealer_card.value() < 10 and dealer_card.rank != Ranks.A:
+                            return Actions.DOUBLE
+                    case 11:
+                        # always double 11
                         return Actions.DOUBLE
+
+        # hard value hands
+        if player_type == 'hard':
+            if player_value <= 11:
+                # if we haven't hit the above double scenarios, hit any hand 11 or less.
+                return Actions.HIT
+            elif player_value == 12:
+                # hit against a dealer 2/3
+                if dealer_card.value() in [2, 3]:
                     return Actions.HIT
 
-                if player_value == 11:
-                    return Actions.DOUBLE
+            # if we are less than 17 and expected dealer value is >= 17, hit
+            if player_value < 17 and dealer_value >= 17:
+                print(f'dealer_card={dealer_card} player={player.cards[0]} {player.cards[1]}, ' \
+                      f'player_value={player_value} dealer_value={dealer_value}, so returning HIT')
+                return Actions.HIT
 
-        # always stand on hard 17+
-        if player_type == 'hard' and player_value >= 17:
-            return Actions.STAND
+            print(f'dealer_card={dealer_card} player={player.cards[0]} {player.cards[1]}, ' \
+                  f'player_value={player_value} dealer_value={dealer_value}, so returning STAND')
 
-        # if we didn't meet any of the above scenarios, just hit
-        return Actions.HIT
+        # if we didn't meet any of the above scenarios, just stand
+        return Actions.STAND
--- tests/test_blackjack_8_hit.py
+++ tests/test_blackjack_8_hit.py
@@ -40,7 +40,7 @@ class TestHand:
             check.equal(
                 action,
                 expected_action,
-                f'{player.value()} with {dealer.cards[0].rank}, should {action.name}',
+                f'{player.value()} against {dealer.cards[0].rank}, should {expected_action.name}',
             )
 
 
@@ -117,3 +117,89 @@ class TestHand:
         assert player.value() == ('hard', 11)
 
         self.compare_actions(player, [Actions.DOUBLE] * 10)
+
+    # hard 12: stay on dealer 4-7, hit on the rest
+    def test_player_hard_12(self):
+        player = Hand([Card(Ranks._2), Card(Ranks.T)])
+        assert player.value() == ('hard', 12)
+
+        self.compare_actions(player, [
+            Actions.HIT,    # dealer 2
+            Actions.HIT,    # dealer 3
+            Actions.STAND,  # dealer 4
+            Actions.STAND,  # dealer 5
+            Actions.STAND,  # dealer 6
+            Actions.HIT,    # dealer 7
+            Actions.HIT,    # dealer 8
+            Actions.HIT,    # dealer 9
+            Actions.HIT,    # dealer T
+            Actions.HIT,    # dealer A
+        ])
+
+    # hard 13: stand against bustable dealer hand, hit against made dealer hands
+    def test_player_hard_13(self):
+        player = Hand([Card(Ranks._3), Card(Ranks.T)])
+        assert player.value() == ('hard', 13)
+
+        self.compare_actions(player, self.flatten([
+            [Actions.STAND] * 5,  # dealer 2-6
+            [Actions.HIT] * 5,    # dealer 7-A
+        ]))
+
+    # hard 14: stand against bustable dealer hand, hit against made dealer hands
+    def test_player_hard_14(self):
+        player = Hand([Card(Ranks._4), Card(Ranks.T)])
+        assert player.value() == ('hard', 14)
+
+        self.compare_actions(player, self.flatten([
+            [Actions.STAND] * 5,  # dealer 2-6
+            [Actions.HIT] * 5,    # dealer 7-A
+        ]))
+
+    # hard 15: stand against bustable dealer hand, hit against made dealer hands
+    def test_player_hard_15(self):
+        player = Hand([Card(Ranks._5), Card(Ranks.T)])
+        assert player.value() == ('hard', 15)
+
+        self.compare_actions(player, self.flatten([
+            [Actions.STAND] * 5,  # dealer 2-6
+            [Actions.HIT] * 5,    # dealer 7-A
+        ]))
+
+    # hard 16: stand against bustable dealer hand, hit against made dealer hands
+    def test_player_hard_16(self):
+        player = Hand([Card(Ranks._6), Card(Ranks.T)])
+        assert player.value() == ('hard', 16)
+
+        self.compare_actions(player, self.flatten([
+            [Actions.STAND] * 5,  # dealer 2-6
+            [Actions.HIT] * 5,    # dealer 7-A
+        ]))
+
+    # hard 17: stand against anything
+    def test_player_hard_17(self):
+        player = Hand([Card(Ranks._7), Card(Ranks.T)])
+        assert player.value() == ('hard', 17)
+
+        self.compare_actions(player, [Actions.STAND] * 10)
+
+    # hard 18: stand against anything
+    def test_player_hard_18(self):
+        player = Hand([Card(Ranks._8), Card(Ranks.T)])
+        assert player.value() == ('hard', 18)
+
+        self.compare_actions(player, [Actions.STAND] * 10)
+
+    # hard 19: stand against anything
+    def test_player_hard_19(self):
+        player = Hand([Card(Ranks._9), Card(Ranks.T)])
+        assert player.value() == ('hard', 19)
+
+        self.compare_actions(player, [Actions.STAND] * 10)
+
+    # hard 20: stand against anything
+    def test_player_hard_20(self):
+        player = Hand([Card(Ranks.K), Card(Ranks.T)])
+        assert player.value() == ('hard', 20)
+
+        self.compare_actions(player, [Actions.STAND] * 10)
